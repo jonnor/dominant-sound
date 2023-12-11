f186180ce802ce4fc70ac33fb25c77725b7fe77d
Blake Downward <downward87@gmail.com>
2023-12-12 10:10:05 +1030

    fileutils: Add function to fetch project root

--- src/utils/fileutils.py
+++ src/utils/fileutils.py
@@ -4,14 +4,21 @@ Utilities for dealing with files and directories
 """
 
 import os.path
+from pathlib import Path
+
 
 def ensure_dir(directory):
     if not os.path.exists(directory):
         os.makedirs(directory)
 
+
 def ensure_dir_for_file(path):
     directory = os.path.dirname(path)
     if directory:
         ensure_dir(directory)
+
+
+def get_project_root() -> Path:
+    return Path(__file__).parent.parent.parent
 
 
